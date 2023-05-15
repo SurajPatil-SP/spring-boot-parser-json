1a6e54f52b90f9533b6a4d50d669242d5e58e0ca
Sharvil <you@example.com>
2023-05-15 15:16:36 +0530

    Added Dependency for JSON.simple and Demo Examples of JSON Parsers

--- src/main/java/com/main/sbp/controller/ParseController.java
+++ src/main/java/com/main/sbp/controller/ParseController.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.main.sbp.data.DataItem;
 import com.main.sbp.data.DataObject;
+import com.main.sbp.data.Person;
 
 @RestController
 public class ParseController {
@@ -33,6 +37,17 @@ public class ParseController {
 				.collect(Collectors.toList());
 
 		System.out.println(ids);
+		
+		// Demo Example
+		String jsonString1 = "{\"name\":\"John\",\"age\":30}";
+
+		Person person = gson.fromJson(jsonString1, Person.class);
+
+		String name = person.getName();
+		int age = person.getAge();
+
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
 
 	}
 
@@ -55,7 +70,42 @@ public class ParseController {
 		jsonNode.get("data")
 			.elements()
 			.forEachRemaining(element -> System.out.println(element.get("id").asInt()));
+		
+		// Demo Example
+		String jsonString1 = "{\"name\":\"John\",\"age\":30}";
 
+		try {
+			jsonNode = objectMapper.readTree(jsonString1);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		String name = jsonNode.get("name").asText();
+		int age = jsonNode.get("age").asInt();
+
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+
+	}
+	
+	@GetMapping("/jsonparser")
+	public void jsonParseWithJsonParser () {
+		
+		String jsonString = "{\"name\":\"John\",\"age\":30}";
+
+		JSONParser parser = new JSONParser();
+		JSONObject jsonObject = null;
+		try {
+			jsonObject = (JSONObject) parser.parse(jsonString);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		String name = (String) jsonObject.get("name");
+		Long age = (Long) jsonObject.get("age");
+
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
 	}
 
 }
